795888fd9101b62ffe4039b6845d48b6bb3c7329
Maru <zhangbinhui@souche.com>
2017-08-03 15:14:15 +0800

    Add FlatList to display Gank Item

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
+export default class HomeActivity extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+  componentDidMount() {
+    return fetch('http://gank.io/api/data/all/20/0')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.results
+        }, function() {
+
+        });
+      })
+      .catch((error) => {
+        console.log(error)        
+      });
+  }
+render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 100}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text style={styles.item}>{item.desc}</Text>}
+        />
       </View>
     );
   }
@@ -15,9 +44,11 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
   },
-});
+  item: {
+    padding: 10,
+    fontSize: 18
+  },
+})
